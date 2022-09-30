0b83b5c56d73f43e9f49af696ca7f0eb6df9fa05
shubhm-77 <106855163+shubhm-77@users.noreply.github.com>
2022-09-30 11:57:54 -0400

    Started step 3

--- Controllers/HelloWorldController1.cs
+++ Controllers/HelloWorldController1.cs
@@ -8,17 +8,27 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        /*public string Index()
         {
             return "This is my default action...";
+        }*/
+
+        public IActionResult Index()
+        {
+            return View();
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome()
+        /*public string Welcome()
         {
             return "This is the Welcome action method...";
+        }*/
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         }
     }
 }
--- README.txt
+++ README.txt
@@ -1,2 +1,20 @@
 ﻿30-09-2022
 Assignment started
+created Web App and started program  with ASP.NET core MVC
+using ASP.NET Core 3.1
+Configure for HTTPS
+Start without Debugging and IIS Express
+
+1132
+Added a Controller
+Named HelloWorldController.cs
+Added a Code
+run the program.
+successful
+Changed welcome method and added two parameters
+Step 2 complete & it ran successfully.
+
+1145
+In HelloWorldController.cs, replaced the Index method with the code.
+Added a view
+then added contents in Index.cshtml
